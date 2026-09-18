89264d1bb556d384cfe061577272bc35fda72f23
sumit <sumit@example.com>
2026-09-18 23:08:43 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/xauusd-alert-system/sw.js', { scope: '/xauusd-alert-system/' })})}
